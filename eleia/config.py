3dc10e3aaab48f145df54e949854adf2bcae9e8f
Filin8888 <mi0git0mail@gmail.com>
2025-10-03 14:38:00 +0300

    Delete eleia directory

--- eleia/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://eleia_adm:eleia@localhost:5432/eleia"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret"
